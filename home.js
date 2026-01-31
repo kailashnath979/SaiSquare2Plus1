7770701cedca022ffa22d121f3fb376ec181a67c
Kailash Nath Tallapragada <60371767+kailashnath979@users.noreply.github.com>
2026-01-31 14:56:11 -0800

    Update home.js

--- home.js
+++ home.js
@@ -16,6 +16,11 @@ const slideshowImages = [
   "images/2.JPG",
   "images/3.JPG",
   "images/4.JPG",
+  "images/5.JPG",
+  "images/6.JPG",
+  "images/8.JPG",
+  "images/7.JPG",
+  "images/9.JPG",
 ];
 
 let slideIndex = 0;
